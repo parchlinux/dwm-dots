32156d662ed8052060cea22551519c9d2be2f421
Sohrab Behdani <behdanisohrab@gmail.com>
2024-02-26 11:35:22 +0330

    added dots

--- etc/skel/dwm/autostart.sh
+++ etc/skel/dwm/autostart.sh
@@ -3,7 +3,7 @@ nm-applet &
 dunst &
 lxqt-policykit-agent &
 picom &
-feh --bg-scale /usr/share/wallpapers/parch-os-wallpapers/PArch_dark_wallpaper_1.png &
+feh --bg-scale /usr/share/wallpapers/parch-wallpapers/PArch_dark_wallpaper_1.png &
 nitrogen --restore &
 #fa-key & # enable only after you have had the parch-fakey package installed this is for persian keyboard layout
 volumeicon &
