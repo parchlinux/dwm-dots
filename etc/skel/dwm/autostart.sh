1c1539836fa684801151902b4512cf5bb937ad7a
Sohrab Behdani <behdanisohrab@gmail.com>
2024-02-26 11:34:25 +0330

    added dwm dotfiles

---
+++ etc/skel/dwm/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nm-applet &
+dunst &
+lxqt-policykit-agent &
+picom &
+feh --bg-scale /usr/share/wallpapers/parch-os-wallpapers/PArch_dark_wallpaper_1.png &
+nitrogen --restore &
+#fa-key & # enable only after you have had the parch-fakey package installed this is for persian keyboard layout
+volumeicon &
